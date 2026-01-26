e4d4665f415dbc04fc3e254690366f0691928751
Shubham Dalvi <dalvisd511@gmail.com>
2026-01-26 17:19:12 +0000

    Fix migration order and finalize custom user authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'users',
+    'payments',
+    'transactions',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
